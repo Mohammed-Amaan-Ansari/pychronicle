886805847ef6d9b8b668872e67cd99e8ec9131eb
Mohammed-Amaan-Ansari <ansari.mdamaan1@gmail.com>
2026-07-19 12:45:21 +0530

    Week 2 : Capture runtime frame information

--- examples/trace_demo.py
+++ examples/trace_demo.py
@@ -5,17 +5,29 @@ from pychronicle.tracer.runtime_tracer import (
 
 
 def add(a, b):
+
     result = a + b
+
     return result
+
+
+def multiply(x, y):
+
+    value = x * y
+
+    return value
 
 
 start_tracing()
 
-x = 10
-y = 20
+num1 = 10
 
-z = add(x, y)
+num2 = 20
 
-print(z)
+sum_result = add(num1, num2)
+
+final = multiply(sum_result, 5)
+
+print(final)
 
 stop_tracing()
--- pychronicle/tracer/runtime_tracer.py
+++ pychronicle/tracer/runtime_tracer.py
@@ -1,29 +1,35 @@
 import sys
+from pprint import pprint
 
 
 def trace_function(frame, event, arg):
     """
-    Trace function called by Python for each execution event.
+    Runtime tracing callback.
     """
 
-    print(
-        f"Event: {event:8} | "
-        f"Function: {frame.f_code.co_name:15} | "
-        f"Line: {frame.f_lineno}"
-    )
+    # Ignore tracing inside the tracer itself
+    if frame.f_code.co_filename == __file__:
+        return trace_function
+
+    print("=" * 60)
+
+    print(f"Event      : {event}")
+    print(f"Function   : {frame.f_code.co_name}")
+    print(f"Line       : {frame.f_lineno}")
+    print(f"File       : {frame.f_code.co_filename}")
+
+    print("\nLocal Variables")
+
+    pprint(frame.f_locals)
+
+    print("=" * 60)
 
     return trace_function
 
 
 def start_tracing():
-    """
-    Enable Python tracing.
-    """
     sys.settrace(trace_function)
 
 
 def stop_tracing():
-    """
-    Disable tracing.
-    """
     sys.settrace(None)
